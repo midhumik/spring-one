56d4468b2fb78ea56f1eacf46e08cae2bd9134f8
midhumik <mikpalachi@gmail.com>
2021-01-04 18:56:55 +0530

    First commit

---
+++ simpleWebApp/src/main/java/mik/project/SimpleWebAppApplication.java
@@ -0,0 +1,13 @@
+package mik.project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleWebAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleWebAppApplication.class, args);
+	}
+
+}
---
+++ simpleWebApp/src/test/java/mik/project/SimpleWebAppApplicationTests.java
@@ -0,0 +1,13 @@
+package mik.project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleWebAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
